de63019d5b7ef1d1655a09f4e2ef8df76945a52f
tylerlewis <tyhlewis@gmail.com>
2014-07-24 15:31:50 -0700

    initial commit

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,49 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {
+    storage: {},
+    sizeCount: 0
+  };
+
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+/*var stackMethods = {
+  push: function(value) {
+    storage[size] = value;
+    size++;
+  },
+  pop: function(){
+    if(size > 0) {
+      size--;
+    }
+    var result = storage[size];
+    delete storage[size];
+    return result;
+  },
+  size: function(){
+    return size;
+  }
+};*/
 
-
+var stackMethods = {
+  push: function(value){
+    this.storage[this.sizeCount] = value;
+    this.sizeCount++;
+  },
+  pop: function(){
+    if(this.sizeCount > 0) {
+      this.sizeCount--;
+    }
+    var result = this.storage[this.sizeCount];
+    delete this.storage[this.sizeCount];
+    return result;
+  },
+  size: function(){
+    return this.sizeCount;
+  }
+};
--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,35 @@
 var makeQueue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.sizeCount = 0;
+  return someInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value){
+  this.storage[this.sizeCount] = value;
+  this.sizeCount++;
+};
+
+queueMethods.dequeue = function(){
+  if(this.sizeCount > 0){
+    this.sizeCount--;
+  }
+  var result = this.storage['0'];
+  delete this.storage['0'];
+  for(var key in this.storage){
+    var hold = this.storage[key];
+    var decrease = parseInt(key, 10) - 1;
+    this.storage[decrease] = hold;
+  }
+  return result;
+};
+
+queueMethods.size = function(){
+  return this.sizeCount;
+};
 
 
